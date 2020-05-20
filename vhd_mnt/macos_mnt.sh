8819ff175149d0da2a0d4ceb993099b54196b1b7
leipero <lei.pero@gmail.com>
2020-05-20 08:38:59 +0200

    Add files via upload

---
+++ vhd_mnt/macos_mnt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CFDIRG=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "$CFDIRG/../scripts/config"
+
+## Virtual drive and mount point locations
+VHD_MOUNT_POINT=/home/$(logname)/VHD_MACOS
+
+mkdir -p $VHD_MOUNT_POINT
+sudo modprobe nbd max_part=8
+sudo qemu-nbd --connect=/dev/nbd0 $MACOS_IMG
+#sudo fdisk /dev/nbd0 -l
+wait
+sudo mount /dev/nbd0p2 $VHD_MOUNT_POINT
+exit
